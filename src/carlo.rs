beb0e987af7f98ffba189add610acd689510c557
Davide Mancusi <davide.mancusi@cea.fr>
2018-10-16 22:50:10 +0200

    Major refactoring, split in two threads!

--- src/carlo.rs
+++ src/carlo.rs
@@ -1,50 +1,98 @@
 use std::time::Instant;
+
+use std::sync::mpsc::Sender;
+use std::sync::mpsc;
+
 use std::sync::Arc;
+
+use std::thread;
+
 use irc::client::prelude::{IrcClient, Client, ClientExt, Command};
+use irc::proto::message::Message;
 
 #[derive(Debug)]
 pub struct Carlo {
     start_time: Instant,
+    client: Arc<IrcClient>
 }
+
+
+#[derive(Debug)]
+enum Event {
+    IncomingIrcMessage(Message),
+}
+
 
 impl Carlo {
     pub fn new() -> Carlo {
-        Carlo { start_time: Instant::now() }
+        Carlo {
+            start_time: Instant::now(),
+            client: Arc::new(IrcClient::new("config.toml").unwrap())
+        }
     }
 
     pub fn run(&mut self) {
+        let (tx, rx) = mpsc::channel();
 
-        let client = Arc::new(IrcClient::new("config.toml").unwrap());
-        client.identify().unwrap();
-        self.dispatch(&*client)
-    }
+        self.client.identify().unwrap();
 
-    fn dispatch(&self, client: &IrcClient) {
-        client.for_each_incoming(|irc_msg| {
-            let mut cmd_prefix = "@".to_owned();
-            cmd_prefix.push_str(client.current_nickname());
+        let listener = Listener::new(self.client.clone(), tx);
+        let hlisten = thread::spawn(move || { listener.listen() });
 
-            // irc_msg is a Message
-            match irc_msg.command {
-                Command::PRIVMSG(channel, message) => {
-                    let trimmed = message.trim_left();
-                    if trimmed.starts_with(&cmd_prefix) {
-                        let return_msg = self.handle_msg(&message);
-                        // send_privmsg comes from ClientExt
-                        client.send_privmsg(&channel, return_msg).unwrap();
-                    }
-                },
-                _ => ()
+        for event in rx.iter() {
+            if let Some(message) = self.handle(&event) {
+                self.client.send(message).unwrap();
             }
-        }).unwrap();
+        }
+        hlisten.join().unwrap();
     }
 
-    fn handle_msg(&self, message: &str) -> String {
-        if message.contains("uptime") {
-            format!("uptime: {} seconds",
-                    self.start_time.elapsed().as_secs())
-        } else {
-            "beep boop".to_string()
+    fn handle(&self, event: &Event) -> Option<Message> {
+        match event {
+            Event::IncomingIrcMessage(message) => self.handle_irc(message),
         }
+    }
+
+    fn handle_irc(&self, message: &Message) -> Option<Message> {
+        let cmd_prefix = self.client.current_nickname().to_string();
+        match &message.command {
+            Command::PRIVMSG(_, msg) => {
+                let trimmed = msg.trim_left();
+                if trimmed.starts_with(&cmd_prefix) {
+                    let reply_txt = if msg.contains("uptime") {
+                        format!("uptime = {} seconds",
+                                self.start_time.elapsed().as_secs())
+                    } else {
+                        "beep boop".to_string()
+                    };
+                    let cmd = Command::PRIVMSG(message.response_target().unwrap().to_string(), reply_txt);
+                    let reply = Message::from(cmd);
+                    Some(reply)
+                } else {
+                    None
+                }
+            },
+            _ => None
+        }
+    }
+}
+
+
+
+#[derive(Debug)]
+struct Listener {
+    client: Arc<IrcClient>,
+    tx: Sender<Event>,
+}
+
+impl Listener {
+    fn new(client: Arc<IrcClient>, tx: Sender<Event>) -> Listener {
+        Listener { client, tx }
+    }
+
+    fn listen(&self) {
+        self.client.for_each_incoming(|irc_msg| {
+            self.tx.send(Event::IncomingIrcMessage(irc_msg)).unwrap();
+        }).unwrap();
     }
 }
